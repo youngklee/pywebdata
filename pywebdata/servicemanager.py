bcefaf2a4cfe9cd9c27b8f80fe332815fa0842bd
Damon Rousis <damon.rousis@gmail.com>
2015-08-09 11:25:07 -0500

    cleaned up imports; added initialization parameters and methods

--- pywebdata/servicemanager.py
+++ pywebdata/servicemanager.py
@@ -1,6 +1,6 @@
 import os
 import imp
-import glob
+from glob import glob
 import pkgutil
 
 from baseservice import BaseService
@@ -8,16 +8,22 @@ from exceptions import ServiceNotFoundException
 
 class ServiceManager(object):
 
-    service_dir = os.path.join(os.path.dirname(__file__), 'services')
+    service_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'services'))
     service_dirs = [service_dir]
+    is_initialized = False
 
     def __init__(self):
         map(self._load_services, self.service_dirs)
-        
+        self._set_initialized()
+
     def _load_services(self, dirname):
-        for filename in glob.glob(os.path.join(dirname, '*.py')):
+        for filename in glob(os.path.join(dirname, '*.py')):
             name, ext = os.path.splitext(filename)
             imp.load_source(name, os.path.join(dirname, filename))
+
+    @classmethod
+    def _set_initialized(cls):
+        cls.is_initialized = True
 
     @classmethod
     def add_path(cls, path):
