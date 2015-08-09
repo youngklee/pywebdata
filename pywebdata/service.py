c8bf8b7e9e4dc9bbeda735e38c852822bdc4134a
Damon Rousis <damon.rousis@gmail.com>
2015-08-09 11:24:32 -0500

    removed print line

--- pywebdata/service.py
+++ pywebdata/service.py
@@ -7,7 +7,6 @@ class Service(object):
     def __init__(self, service_string):
         
         self.service = service_manager.activate_service(service_string)
-        print self.service
 
     def query(self, *args, **kwargs):
         self.service.query(*args, **kwargs)
