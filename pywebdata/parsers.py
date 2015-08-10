2efc7501d927f7935c80f956c7d843138b91c1a9
Damon Rousis <drousis@clockwork-solutions.com>
2015-08-10 18:48:20 -0500

    parse_query function returns list of dicts

--- pywebdata/parsers.py
+++ pywebdata/parsers.py
@@ -6,19 +6,28 @@ import operator
 compmap = {
     "<": operator.lt, 
     "<=": operator.le, 
-    "==": operator.eq,
+    "=": operator.eq,
     ">": operator.gt, 
     ">=": operator.ge, 
     "!=": operator.ne
 }
 
+labels = ['name', 'operator', 'value']
 condition_splitter = re.compile(r'\s*(\D\w*)\s*(<=|>=|<|>|!=|=)\s*(\d*\.*\d*)')
 
-def parse_query(qry_str):
-    conditions = qry_str.split('and')
-    return map(lambda x:condition_splitter.match(x).groups(), conditions)
+def parse_query(qry):
+    
+    def make_cond_dict(cond):
+        split_cond = condition_splitter.match(cond).groups()
+        d = {}
+        d['name'] = split_cond[0]
+        d['operator'] = compmap[split_cond[1]]
+        d['value'] = split_cond[2]
+        return d
+        
+    return map(make_cond_dict, qry.split('and'))
     
 if __name__ == '__main__':
 
-    a = parse_query('damon = 5 and m1ke >= 6.0 and b = 203948.09238')
-    print a
+    for i in parse_query('damon = 5 and m1ke >= 6.0 and b = 203948.09238'):
+        print i
