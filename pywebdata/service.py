128124f6ce69eaf3f10743acc764f053f8559ec2
Damon Rousis <damon.rousis@gmail.com>
2015-08-09 20:28:41 -0500

    Service converted to function that returns the desired service

--- pywebdata/service.py
+++ pywebdata/service.py
@@ -2,11 +2,5 @@ from servicemanager import ServiceManager
 
 service_manager = ServiceManager()
 
-class Service(object):
-
-    def __init__(self, service_string):
-        
-        self.service = service_manager.activate_service(service_string)
-
-    def query(self, *args, **kwargs):
-        self.service.query(*args, **kwargs)
+def Service(name):
+    return service_manager.activate_service(name)
