5badb0ed6a13e756e16bdf1c1cd9ff7992cddae7
Damon Rousis <drousis@clockwork-solutions.com>
2015-08-12 14:40:53 -0500

    allow for empty string passed to query_conditional

--- pywebdata/parsers.py
+++ pywebdata/parsers.py
@@ -12,21 +12,25 @@ compmap = {
     "!=": operator.ne
 }
 
-labels = ['name', 'operator', 'value']
 condition_splitter = re.compile(r'\s*(\D\w*)\s*(<=|>=|<|>|!=|=)\s*(\d*\.*\d*)')
 
+def make_cond_dict(cond):
+    split_cond = condition_splitter.match(cond).groups()
+    d = {}
+    d['name'] = split_cond[0]
+    d['operator'] = compmap[split_cond[1]]
+    d['value'] = split_cond[2]
+    return d
+
 def parse_query(qry):
-    
-    def make_cond_dict(cond):
-        split_cond = condition_splitter.match(cond).groups()
-        d = {}
-        d['name'] = split_cond[0]
-        d['operator'] = compmap[split_cond[1]]
-        d['value'] = split_cond[2]
-        return d
-        
-    conditions = map(make_cond_dict, qry.split('and'))
     conditions_by_name = defaultdict(list)
+    
+    try:
+        conditions = map(make_cond_dict, qry.split('and'))
+    except AttributeError:
+        conditions = []
+        
     for c in conditions:
         conditions_by_name[c['name']].append(c)
+        
     return conditions_by_name
